a8b7a5b88519e882fdf02f4cfdb1cafb8f82ca18
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2022-11-05 12:34:30 +0900

    implement simple cbow

---
+++ simple_cbow.py
@@ -0,0 +1,28 @@
+from common.layers import MatMul, SoftmaxWithLoss
+import numpy as np
+import sys
+sys.path.append('..')
+
+
+class SimpleCBOW:
+    def __init__(self, vocab_size, hidden_size):
+        V, H = vocab_size, hidden_size
+        # 重みの初期化
+        W_in = 0.01 * np.random.randn(V, H).astype('f')
+        W_out = 0.01 * np.random.randn(V, H).astype('f')
+
+        # レイヤの生成
+        self.in_layer0 = MatMul(W_in)
+        self.in_layer1 = MatMul(W_in)
+        self.out_layer = MatMul(W_out)
+        self.loss_layer = SoftmaxWithLoss()
+
+        # 全ての重みと勾配をリストにまとめる
+        layers = [self.in_layer0, self.in_layer1, self.out_layer]
+        self.params, self.grads = [], []
+        for layer in layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+        # メンバ変数に単語の分散表現を設定
+        self.word_vecs = W_in
